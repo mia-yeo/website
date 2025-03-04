a800f37c6ebac05a7815a5056a3b5a2e278f9dc0
Mia Yeo <mayeo@uci.edu>
2025-03-04 11:17:34 -0800

    updated hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,11 +145,13 @@
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
       strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
+      loop: false,
+      typeSpeed: 200,
       backSpeed: 50,
       backDelay: 2000
+      
     });
+    
   }
 
   /**
